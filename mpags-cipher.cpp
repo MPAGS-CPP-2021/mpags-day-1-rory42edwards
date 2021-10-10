aaba08c5292d6be1be15514bdde17df8123f95b5
Rory Edwards <u2166501@live.warwick.ac.uk>
2021-10-10 17:06:37 +0000

    Add more command line option functionality

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -10,13 +10,49 @@ for (size_t i{0}; i < cmdLineArgs.size(); i++)
         std::cout << "No help available" << std::endl; // print a help message
         return 0;
     }
+    
+    if (cmdLineArgs[i] == "--version")
+    {
+        std::cout << "v0.1.0" << std::endl; // would need to change this version number as appropriate - can I link directly to the tag instead?
+    }
+
+    if (cmdLineArgs[i] == "-i")
+    {
+        if (cmdLineArgs[cmdLineArgs.size()-1] == "-i") // checks if -i has something after it (where the input file should be) and if not, exits the program
+        {
+            std::cout << "No input file was specified" << std::endl;
+            return 0;
+        }
+        else
+        {
+            std::string input_file{cmdLineArgs[i+1]};
+            std::cout << "Input file: " + input_file << std::endl;
+        }
+        
+    }
+
+    if (cmdLineArgs[i] == "-o")
+    {
+        if (cmdLineArgs[cmdLineArgs.size()-1] == "-o") // checks if -o has something after it (where the output file should be) and if not, exits the program
+        {
+            std::cout << "No output file was specified" << std::endl;
+            return 0;
+        }
+        else
+        {
+            std::string output_file{cmdLineArgs[i+1]};
+            std::cout << "Output file: " + output_file << std::endl;
+        }
+
+    }
+
 }
 
 char in_char{'x'};
 std::string in_char_letters{""};
 while(std::cin >> in_char)
 {    
-    if (std::isalpha(in_char)) // check for letter
+    if (std::isalpha(in_char)) // check if in_char is a letter
     {
         in_char_letters += std::toupper(in_char); // make letter uppercase
     }
